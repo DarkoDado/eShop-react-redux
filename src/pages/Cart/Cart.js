531e3472110912a18e8c77b349fc695b09b8a562
Darko Pantic <gusar97@gmail.com>
2023-01-06 00:36:04 +0100

    setup converter(currency) total price functionality

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,98 +1,154 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { add_to_cart, clear_cart, decrease_cart, remove_from_cart, selectCartItems, total_quantity } from "../../redux/slice/cartSlice";
-import styles from "./Cart.module.css"
-import {FiDelete} from "react-icons/fi"
+import {
+  add_to_cart,
+  cartTotalQuantity,
+  cart_subtotal,
+  clear_cart,
+  currency_converter,
+  decrease_cart,
+  remove_from_cart,
+  selectCartAmount,
+  selectCartItems,
+  total_quantity,
+} from "../../redux/slice/cartSlice";
+import styles from "./Cart.module.css";
+import { FiDelete } from "react-icons/fi";
 
 const Cart = () => {
-   const cartItems = useSelector(selectCartItems)
-  const dispatch = useDispatch()
 
-   const addToCart = (item) => {
+  const [convert, setConvert] = useState("usd")
+
+  const cartItems = useSelector(selectCartItems);
+  const cartTotalAmount = useSelector(selectCartAmount)
+  const totalQuantity = useSelector(cartTotalQuantity)
+  const dispatch = useDispatch();
+
+  const addToCart = (item) => {
     dispatch(add_to_cart(item));
   };
   const decrease = (item) => {
-    dispatch(decrease_cart(item))
-  }
+    dispatch(decrease_cart(item));
+  };
   const removeProduct = (item) => {
-    dispatch(remove_from_cart(item))
-  }
-  
+    dispatch(remove_from_cart(item));
+  };
+
   useEffect(() => {
-    dispatch(total_quantity())
-  }, [cartItems, dispatch])
+    dispatch(total_quantity());
+    dispatch(cart_subtotal())
+    dispatch(currency_converter({convert, cartTotalAmount}))
+  }, [cartItems, dispatch, convert]);
+
+  const swichConvert = () => {
+    switch(convert) {
+    case "usd" : 
+    return <>$</>
+    case "eur" : 
+    return <>EUR</>
+    case "rsd" :
+      return <>RSD</>
+    case "bam" :
+      return <>BAM</>
+    default: <></>
+  }
+  }
 
   return (
     <div>
-    {cartItems.length === 0 ? (
-      <h2>Your cart is empty...</h2>
-    )
-  : (
-    <>
-    <table className={styles.table}>
-      <thead className={styles.tHead}>
-        <tr className={styles.tableRow}>
-          <th>S/N</th>
-          <th>Product</th>
-          <th>Price</th>
-          <th>Quantity</th>
-          <th>Total</th>
-          <th></th>
-        </tr>
-      </thead>
-    
-    <tbody>
-    {cartItems.map((item,index) => {
-        const {id, image, title, price, cartQuantity} = item
-        return (
-          <>
-          <tr key={index}>
-          <td>{index + 1}</td>
+      {cartItems.length === 0 ? (
+        <h2>Your cart is empty...</h2>
+      ) : (
+        <>
+          <table className={styles.table}>
+            <thead className={styles.tHead}>
+              <tr className={styles.tableRow}>
+                <th>S/N</th>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total</th>
+                <th></th>
+              </tr>
+            </thead>
 
-          <td><p>{title}</p>
-          <img src={image} style={{width: "50px", height: "60px"}} alt="product image" /> 
-          </td>
-          <td>{price}$</td>
-          <td>
-          <button onClick={() => decrease(item)} className={styles["increase-decrease"]}>-</button>
-          {cartQuantity}
-          <button
-                    onClick={() => addToCart(item)}
-                    className={styles["increase-decrease"]}
-                  >
-                    +
-                  </button>
+            <tbody>
+              {cartItems.map((item, index) => {
+                const { id, image, title, price, cartQuantity } = item;
+                return (
+                  <>
+                    <tr key={index}>
+                      <td>{index + 1}</td>
+
+                      <td>
+                        <p>{title}</p>
+                        <img
+                          src={image}
+                          style={{ width: "50px", height: "60px" }}
+                          alt="product image"
+                        />
+                      </td>
+                      <td>{price}$</td>
+                      <td>
+                        <button
+                          onClick={() => decrease(item)}
+                          className={styles["increase-decrease"]}
+                        >
+                          -
+                        </button>
+                        {cartQuantity}
+                        <button
+                          onClick={() => addToCart(item)}
+                          className={styles["increase-decrease"]}
+                        >
+                          +
+                        </button>
+                      </td>
+                      <td>{(price * cartQuantity).toFixed(2)}$</td>
+                      <td>
+                        <FiDelete
+                          size={24}
+                          color="red"
+                          onClick={() => removeProduct(item)}
+                        />
+                      </td>
+                    </tr>
+                  </>
+                );
+              })}
+            </tbody>
+          </table>
+          <div className="container">
+            <div className={styles.checkoutDetails}>
+              <span>
+                <button
+                  className={styles.clearBtn}
+                  onClick={() => dispatch(clear_cart())}
+                >
+                  Clear All
+                </button>
+              </span>
+              <span>
+                <h4>Cart items: {totalQuantity}</h4>
+                <h3>Total price: <b>{cartTotalAmount.toFixed(2)}</b>{swichConvert()}
+              </h3>
+              </span>
+              <span className={styles.convert}>
+              <h4>Convert total price</h4>
+                <select value={convert}
+                onChange={(e) => setConvert(e.target.value)}>
                   
-                  
-                  
-          </td>
-          <td>25252</td>
-          <td><FiDelete size={24} color="red" onClick={() => removeProduct(item)}/></td>
-          </tr>
-          
-          </>
-        )
-      })}
-      </tbody>
-      
-      </table>
-      <div className="container">
-      <div className={styles.checkoutDetails}>
-        <span>
-      <button className={styles.clearBtn} onClick={() => dispatch(clear_cart())}>Clear All</button>
-      </span>
-      <span>
-        <h3>Cart items:</h3>
-        <h2>Subtotal: </h2>
-        </span>
-      </div>
-      </div>
-   
-      
-     </>
-    )}
-   
-  </div>
+                  <option value="usd">USD</option>
+                  <option value="eur">EUR</option>
+                  <option value="rsd">RSD</option>
+                  <option value="bam">BAM</option>
+                </select>
+              </span>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 
--- src/redux/slice/cartSlice.js
+++ src/redux/slice/cartSlice.js
@@ -29,7 +29,7 @@ const cartSlice = createSlice({
       );
       if (state.cartItems[indexOfProduct].cartQuantity > 1) {
         state.cartItems[indexOfProduct].cartQuantity -= 1;
-      } else if (state.cartItems[indexOfProduct].cartQuantity === 1 ) {
+      } else if (state.cartItems[indexOfProduct].cartQuantity === 1) {
         const filteredProduct = state.cartItems.filter(
           (product) => product.id !== action.payload.id
         );
@@ -49,18 +49,58 @@ const cartSlice = createSlice({
       state.totalQuantity = cartTotalQuantity;
     },
     remove_from_cart: (state, action) => {
-        const removedProduct = state.cartItems.filter(item => item.id !== action.payload.id)
-        state.cartItems = removedProduct
-      },
+      const removedProduct = state.cartItems.filter(
+        (item) => item.id !== action.payload.id
+      );
+      state.cartItems = removedProduct;
+    },
     clear_cart: (state) => {
-        state.cartItems = []
+      state.cartItems = [];
+    },
+    cart_subtotal: (state, action) => {
+      const array = [];
+      state.cartItems.map((item) => {
+        const { cartQuantity, price } = item;
+        const subtotal = cartQuantity * price;
+        array.push(subtotal);
+      });
+      const cartAmountTotal = array.reduce((a, b) => {
+        return a + b;
+      }, 0);
+      state.totalAmount = cartAmountTotal;
+    },
+currency_converter: (state, action) => {
+    const {convert} = action.payload
+    let converted = []
+    if(convert === "usd") {
+        converted = state.totalAmount
     }
+    if(convert === "eur") {
+        converted = state.totalAmount * 0.95035
+    }
+    if(convert === "rsd") {
+        converted = state.totalAmount * 111.5
+    }
+    if(convert === "bam") {
+        converted = state.totalAmount * 1.8588
+    }
+    state.totalAmount = converted
+},
   },
- 
 });
 
-export const { add_to_cart, total_quantity, decrease_cart, remove_from_cart, clear_cart } = cartSlice.actions;
+export const {
+  add_to_cart,
+  total_quantity,
+  decrease_cart,
+  remove_from_cart,
+  clear_cart,
+  cart_subtotal,
+  currency_converter
+} = cartSlice.actions;
 
 export const selectCartItems = (state) => state.cart.cartItems;
+export const selectCartAmount = (state) => state.cart.totalAmount
+export const cartTotalQuantity = state => state.cart.totalQuantity
 
 export default cartSlice.reducer;
